0d2c427ebba4e2fd4f5677753ada273ef8f07d82
theZackD <dr.zakaria95@gmail.com>
2023-10-18 17:28:10 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Changes to be committed:
    	deleted:    .gitignore
    	deleted:    rock-papaper-scissors/README.md
    	deleted:    rock-papaper-scissors/rock-paper-scissors.fig
    	deleted:    rock-papaper-scissors/src/App.css
    	deleted:    rock-papaper-scissors/src/App.jsx
    	deleted:    rock-papaper-scissors/src/index.css
    	deleted:    rock-papaper-scissors/style-guide.md
    	renamed:    rock-papaper-scissors/.eslintrc.cjs -> rock-paper-scissors/.eslintrc.cjs
    	renamed:    rock-papaper-scissors/.gitignore -> rock-paper-scissors/.gitignore
    	new file:   rock-paper-scissors/README.md
    	renamed:    rock-papaper-scissors/index.html -> rock-paper-scissors/index.html
    	renamed:    rock-papaper-scissors/package-lock.json -> rock-paper-scissors/package-lock.json
    	renamed:    rock-papaper-scissors/package.json -> rock-paper-scissors/package.json
    	renamed:    rock-papaper-scissors/public/vite.svg -> rock-paper-scissors/public/vite.svg
    	new file:   rock-paper-scissors/src/App.css
    	renamed:    rock-papaper-scissors/src/images/bg-pentagon.svg -> rock-paper-scissors/src/assets/images/bg-pentagon.svg
    	renamed:    rock-papaper-scissors/src/images/bg-triangle.svg -> rock-paper-scissors/src/assets/images/bg-triangle.svg
    	renamed:    rock-papaper-scissors/src/images/favicon-32x32.png -> rock-paper-scissors/src/assets/images/favicon-32x32.png
    	renamed:    rock-papaper-scissors/src/images/icon-close.svg -> rock-paper-scissors/src/assets/images/icon-close.svg
    	renamed:    rock-papaper-scissors/src/images/icon-lizard.svg -> rock-paper-scissors/src/assets/images/icon-lizard.svg
    	renamed:    rock-papaper-scissors/src/images/icon-paper.svg -> rock-paper-scissors/src/assets/images/icon-paper.svg
    	renamed:    rock-papaper-scissors/src/images/icon-rock.svg -> rock-paper-scissors/src/assets/images/icon-rock.svg
    	renamed:    rock-papaper-scissors/src/images/icon-scissors.svg -> rock-paper-scissors/src/assets/images/icon-scissors.svg
    	renamed:    rock-papaper-scissors/src/images/icon-spock.svg -> rock-paper-scissors/src/assets/images/icon-spock.svg
    	renamed:    rock-papaper-scissors/src/images/image-rules-bonus.svg -> rock-paper-scissors/src/assets/images/image-rules-bonus.svg
    	renamed:    rock-papaper-scissors/src/images/image-rules.svg -> rock-paper-scissors/src/assets/images/image-rules.svg
    	renamed:    rock-papaper-scissors/src/images/logo-bonus.svg -> rock-paper-scissors/src/assets/images/logo-bonus.svg
    	renamed:    rock-papaper-scissors/src/images/logo.svg -> rock-paper-scissors/src/assets/images/logo.svg
    	renamed:    rock-papaper-scissors/src/assets/react.svg -> rock-paper-scissors/src/assets/react.svg
    	new file:   rock-paper-scissors/src/components/Header.tsx
    	renamed:    rock-papaper-scissors/src/main.jsx -> rock-paper-scissors/src/main.tsx
    	new file:   rock-paper-scissors/src/vite-env.d.ts
    	new file:   rock-paper-scissors/tsconfig.json
    	new file:   rock-paper-scissors/tsconfig.node.json
    	renamed:    rock-papaper-scissors/vite.config.js -> rock-paper-scissors/vite.config.ts

---
+++ rock-paper-scissors/src/components/Header.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react'
+
+interface HProps {
+    gameMode : string;
+    score : number;
+}
+
+const Header = ({gameMode, score} : HProps) => {
+  return (
+    <>
+      <header>
+          <p className='Title'>{gameMode}</p>
+          <div className='Score-container'>
+            <p id='scoreLabel'>Score</p>
+            <p id='Score'>{score}</p>
+          </div>
+        </header>
+    </>
+  )
+}
+
+export default Header
